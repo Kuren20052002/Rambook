e7228ab217498b7cac9e0cf2e0e794fd75f61bad
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-12-14 09:12:12 +0700

    Add post#create and create post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post created succesfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Failed to create post"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +31,11 @@ class PostsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :image)
   end
 end
